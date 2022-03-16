1e07c06bbe4a4e7bdabdf289cad5da9a72041ce5
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-15 20:43:32 -0400

    Remove redundant header and main tags

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -4,19 +4,8 @@ import { MetaTags } from '@redwoodjs/web'
 const HomePage = () => {
   return (
     <>
-       <MetaTags title="Home" description="Home page" />
-
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>Home</main>
+      <MetaTags title="Home" description="Home page" />
+      Home
     </>
   )
 }
