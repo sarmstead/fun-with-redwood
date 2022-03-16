3c2a08e92a44221d05f8adad40818fffb6027cce
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-15 21:06:16 -0400

    Remove extra Home link and imports

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,4 +1,3 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const AboutPage = () => {
@@ -9,7 +8,6 @@ const AboutPage = () => {
         This site was created to demonstrate my mastery of Redwood: Look on my
         works, ye mighty, and despair!
       </p>
-      <Link to={routes.home()}>Return home</Link>
     </>
   )
 }
