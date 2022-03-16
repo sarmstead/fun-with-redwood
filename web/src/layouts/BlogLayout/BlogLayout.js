00a4fe6ee62c7bc44c10b13d82fef4361ec9f12e
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-15 21:04:38 -0400

    Add links to home page in blog layout

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -3,9 +3,12 @@ import { Link, routes } from '@redwoodjs/router'
 const BlogLayout = ({ children }) => {
   return <>
     <header>
-      <h1>Redwood Blog</h1>
+      <h1><Link to={routes.home()}>Redwood Blog</Link></h1>
       <nav>
         <ul>
+          <li>
+            <Link to={routes.home()}>Home</Link>
+          </li>
           <li>
             <Link to={routes.about()}>About</Link>
           </li>
