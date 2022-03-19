51936e5a1893a12a4e8908fe5d62af1ffc379a56
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-19 14:59:59 -0400

    Refactor Success component

--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -18,11 +18,13 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ articles }) => {
-  return (
-    <ul>
-      {articles.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
-    </ul>
-  )
+  return articles.map((article) => (
+    <article key={article.id}>
+      <header>
+        <h2>{article.title}</h2>
+      </header>
+      <p>{article.body}</p>
+      <div>Posted at: {article.createdAt}</div>
+    </article>
+  ))
 }
