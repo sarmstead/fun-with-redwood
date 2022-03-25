850ae79c5feeb7a152bed716d25f476ac13ad0ca
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-25 14:09:08 -0400

    Conditionally render Article based on props

--- web/src/components/Article/Article.js
+++ web/src/components/Article/Article.js
@@ -1,10 +1,32 @@
-const Article = () => {
-  return (
-    <div>
-      <h2>{'Article'}</h2>
-      <p>{'Find me in ./web/src/components/Article/Article.js'}</p>
-    </div>
-  )
+import { Link, routes } from '@redwoodjs/router';
+
+const Article = ({ allArticles, thisArticle }) => {
+  if (allArticles) {
+    return allArticles.map((article) => (
+      <article key={article.id}>
+        <header>
+          <h2>
+            <Link to={routes.article({id: article.id})}>{article.title}</Link>
+          </h2>
+        </header>
+        <p>{article.body}</p>
+        <div>Posted at: {article.createdAt}</div>
+      </article>
+    ))
+  } else if (thisArticle) {
+    return (
+      <article key={thisArticle.id}>
+        <header>
+          <h2>
+            {thisArticle.title}
+          </h2>
+        </header>
+        <p>{thisArticle.body}</p>
+        <div>Posted at: {thisArticle.createdAt}</div>
+      </article>
+    )
+  }
+
 }
 
 export default Article
