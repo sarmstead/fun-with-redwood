3d9ffb42799edef10be1708253e173407b586d7b
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-25 13:23:15 -0400

    Remove Link component

--- web/src/pages/ArticlePage/ArticlePage.js
+++ web/src/pages/ArticlePage/ArticlePage.js
@@ -11,8 +11,7 @@ const ArticlePage = () => {
         Find me in <code>./web/src/pages/ArticlePage/ArticlePage.js</code>
       </p>
       <p>
-        My default route is named <code>article</code>, link to me with `
-        <Link to={routes.article()}>Article</Link>`
+        My default route is named <code>article</code>`
       </p>
     </>
   )
