5eff4f13ae581a830e281723075547f2454d9519
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-25 16:24:24 -0400

    Replace boilerplate with code from RW docs

--- api/src/services/contacts/contacts.js
+++ api/src/services/contacts/contacts.js
@@ -3,3 +3,16 @@ import { db } from 'src/lib/db'
 export const contacts = () => {
   return db.contact.findMany()
 }
+
+export const contact = ({ id }) => {
+  return db.contact.findUnique({
+    where: { id },
+  })
+}
+
+export const createContact = ({ input }) => {
+  return db.contact.create({
+    data: input,
+  })
+}
+
