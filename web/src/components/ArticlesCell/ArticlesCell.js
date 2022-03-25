b42c8d4124d2d3dc82d57e3ae5afeff07644bacb
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-25 13:08:11 -0400

    Wrap article title in a Link tag

--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -1,3 +1,4 @@
+import { Link, routes } from '@redwoodjs/router';
 export const QUERY = gql`
   query ArticlesQuery {
     articles: posts {
@@ -21,7 +22,9 @@ export const Success = ({ articles }) => {
   return articles.map((article) => (
     <article key={article.id}>
       <header>
-        <h2>{article.title}</h2>
+        <h2>
+          <Link to={routes.article()}>{article.title}</Link>
+        </h2>
       </header>
       <p>{article.body}</p>
       <div>Posted at: {article.createdAt}</div>
