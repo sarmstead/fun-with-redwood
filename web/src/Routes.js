8505de4b98e59ab9f38ead593e4dfee15c1d09a2
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-25 15:32:45 -0400

    Generate contact page

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -14,6 +14,7 @@ import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/contact" page={ContactPage} name="contact" />
       <Set wrap={PostsLayout}>
         <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
