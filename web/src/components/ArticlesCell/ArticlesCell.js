dffc53b9206eb346682fc1796397a2d6f34b7d55
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-25 14:10:44 -0400

    Import and use Article component

--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -1,4 +1,4 @@
-import { Link, routes } from '@redwoodjs/router';
+import Article from 'src/components/Article/'
 export const QUERY = gql`
   query ArticlesQuery {
     articles: posts {
@@ -19,15 +19,5 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ articles }) => {
-  return articles.map((article) => (
-    <article key={article.id}>
-      <header>
-        <h2>
-          <Link to={routes.article({id: article.id})}>{article.title}</Link>
-        </h2>
-      </header>
-      <p>{article.body}</p>
-      <div>Posted at: {article.createdAt}</div>
-    </article>
-  ))
+  return <Article allArticles={articles} />
 }
