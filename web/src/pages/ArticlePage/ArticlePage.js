f22e15a4008b9a09e7851b27128540cc76f98ae3
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-25 13:45:42 -0400

    Import and return ArticleCell

--- web/src/pages/ArticlePage/ArticlePage.js
+++ web/src/pages/ArticlePage/ArticlePage.js
@@ -1,18 +1,12 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import ArticleCell from 'src/components/ArticleCell'
 
-const ArticlePage = () => {
+const ArticlePage = ( { id } ) => {
   return (
     <>
       <MetaTags title="Article" description="Article page" />
-
-      <h1>ArticlePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ArticlePage/ArticlePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>article</code>`
-      </p>
+      <ArticleCell id={id} />
     </>
   )
 }
