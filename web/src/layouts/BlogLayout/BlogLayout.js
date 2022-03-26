f48c52663580f721884dc2ab504a9e455b9ce33f
Sunjay Armstead <connect@sunjayarmstead.local>
2022-03-26 14:31:40 -0400

    Remove console log

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -3,7 +3,6 @@ import { Link, routes } from '@redwoodjs/router'
 
 const BlogLayout = ({ children }) => {
   const {isAuthenticated, currentUser, logOut} = useAuth();
-  console.log(currentUser);
   return <>
     <header>
       <div className="flex-between">
